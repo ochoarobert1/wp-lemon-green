d989a6d3b26429aee47f4156de47c26169daa36a
Robert Ochoa <ochoa.robert1@gmail.com>
2024-04-13 23:15:13 -0400

    Add Sass - Dev Tools

--- source/themes/lemon-green-theme/functions.php
+++ source/themes/lemon-green-theme/functions.php
@@ -7,30 +7,61 @@
  *
  * https://developers.elementor.com/docs/hello-elementor-theme/
  *
- * @package HelloElementorChild
- */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
-
-/**
- * Load child theme scripts & styles.
+ * @package LemonGreenTheme
+ * @author Robert Ochoa
+ * @since 1.0.0
  *
- * @return void
  */
-function hello_elementor_child_scripts_styles() {
 
-	wp_enqueue_style(
-		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[
-			'hello-elementor-theme-style',
-		],
-		HELLO_ELEMENTOR_CHILD_VERSION
-	);
-
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+define('LEMON_GREEN_CHILD_VERSION', '1.0.0');
+
+
+if (! class_exists('LemonGreenTheme')) {
+    /**
+     * LemonGreenTheme
+    */
+    class LemonGreenTheme
+    {
+        /**
+         * Method __construct
+         *
+         * @return void
+         */
+        public function __construct()
+        {
+            add_action('wp_enqueue_scripts', [$this, 'enqueueScriptsStyles'], 99);
+        }
+
+        /**
+         * Method enqueueScriptsStyles
+         *
+         * @return void
+         */
+        public function enqueueScriptsStyles()
+        {
+            wp_enqueue_style(
+                'lemongreen-child-style',
+                get_stylesheet_directory_uri() . '/style.css',
+                [],
+                LEMON_GREEN_CHILD_VERSION,
+                'all'
+            );
+
+            wp_enqueue_script(
+                'lemongreen-child-script',
+                get_stylesheet_directory_uri() . '/functions.js',
+                [],
+                LEMON_GREEN_CHILD_VERSION,
+                ['in_footer' => true]
+            );
+            wp_dequeue_style('wp-block-library');
+            wp_dequeue_style('wp-block-library-theme');
+        }
+    }
+
+    new LemonGreenTheme();
+}
